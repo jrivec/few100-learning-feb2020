fa1a52557f3361589adb0a7d26f6a669b76001ce
jrivec <61019020+jrivec@users.noreply.github.com>
2020-02-17 20:57:14 +0000

    DoneMonday 2/17

---
+++ src/math.ts
@@ -0,0 +1,9 @@
+export function add(a: number, b: number) {
+    // tslint:disable-next-line: no-unused-expression
+    return a + b;
+}
+
+function subtract(a: number, b: number) {
+    // tslint:disable-next-line: no-unused-expression
+    return a - b;
+}
